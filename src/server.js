baf14a97bb36e7bd81a255cc8d77d007dd63e48a
Michael Reynolds <michael4reynolds@gmail.com>
2016-06-12 08:12:58 -0400

    change port for heroku

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use('*', (req, res) => {
   res.status(404).json({ message: 'Not Found' })
 })
 
-app.listen(8000, () => {
+  app.listen(process.env.PORT || 8000, () => {
   console.log('Listening on port 8000')
 })
 
